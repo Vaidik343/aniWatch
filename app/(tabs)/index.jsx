22ff5c4e4741ebd5e04c4016a0a4f0e0c4b49dfa
Vaidik343 <vaidik.bbcspl@gmail.com>
2025-09-05 13:25:41 +0530

    working bottom bar and screen routing

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,16 +1,17 @@
 // app/(tabs)/index.tsx
-import { useRoute } from '@react-navigation/native';
-import { Text, View } from 'react-native';
-
+import { router } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Home() {
-
-  
   return (
-    <View>
-      <Text>Welcome to AniWatch Home</Text>
+    <View className="flex-1 justify-center  bg-[#020617]">
+      <View className="flex-1 justify-center mt-8 flex-row gap-3 ">
+        <Text  className='color-white'    onPress={() => router.push("/")}>Home</Text>
+        <Text className='color-white' onPress={() => router.push("/home/upcoming")}>Upcoming</Text>
+        <Text className='color-white' onPress={() => router.push("/home/movie")}>Movies</Text>
+        <Text className='color-white'  onPress={() => router.push("/home/manga")}>Manga</Text>
+        <Text className='color-white'  onPress={() => router.push("/home/suggestion")}>Suggetions</Text>
+      </View>
     </View>
   );
 }
-
-
